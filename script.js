e2dddb224fccaaca029f67b75e619c126a154bd1
Denise-Dichiara <dichiara.d@gmail.com>
2024-05-28 21:33:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+//console.log("Hello Sun");
+
+let hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener("click", function() {
+    document.body.classList.toggle('open');
+});
